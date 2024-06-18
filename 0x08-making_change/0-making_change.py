cde5e068f21a6702fc16aa64013248e3d27a2c33
yahia <yahyaoulha0@gmail.com>
2024-06-18 19:35:51 +0100

    worked

---
+++ 0x08-making_change/0-making_change.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+
+
+def makeChange(coins, total):
+    """
+    get the the fewest number of coins needed to meet a given amount
+    """
+    coins.sort()
+    TotalCoins = [0] * (total + 1)
+
+    for i in range(1, total + 1):
+        mini = float('inf')
+
+        for coin in coins:
+            diff = i - coin
+            if diff < 0:
+                break
+
+            mini = min(mini, TotalCoins[diff] + 1)
+        TotalCoins[i] = mini
+
+    if TotalCoins[total] < float('inf'):
+        return TotalCoins[total]
+    return -1
